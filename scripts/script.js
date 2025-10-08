0352c2d1fbeaa73f90297eb43b4884c475515f99
MathijsN <72303334+MathijsN@users.noreply.github.com>
2025-10-08 16:59:08 +0200

    fixed #21

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,14 @@
 const menuButton = document.querySelector("header > img")
 const nav = document.querySelector("nav")
+const header = document.querySelector("header")
+const flyingButton = document.querySelector(".flying-button")
 
 menuButton.addEventListener('click', toggleMenu)
 
 function toggleMenu () {
     nav.classList.toggle('open')
+    header.classList.toggle('static')
+    flyingButton.classList.toggle('display')
     console.log("clicked")
 }
 
